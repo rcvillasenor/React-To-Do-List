d7eec8a261f3294cffe754d8f70dcb07ad90e8f1
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-04-27 03:25:02 -0700

    jsx to-do app framing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+import "./styles.css";
+
 export default function App() {
-  return "Hi";
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"></input>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To-do List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
