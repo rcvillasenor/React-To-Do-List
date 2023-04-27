88686b8783683540c8778e52e68fa428b5b7ed5a
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-04-27 03:43:42 -0700

    Updating jsx app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./styles.css";
 
 export default function App() {
+  /*const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+  }
+
+  console.log(todos)*/
+
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item"></input>
+          <input /*value={newItem} onChange={e => setNewItem(e.target.value)}*/ type="text" id="item"></input>
         </div>
         <button className="btn">Add</button>
       </form>
@@ -14,19 +34,19 @@ export default function App() {
       <ul>
         <li>
           <label>
-            <input type="checkbox"/>
+            <input type="checkbox" />
             Item 1
           </label>
           <button className="btn btn-danger">Delete</button>
         </li>
         <li>
           <label>
-            <input type="checkbox"/>
+            <input type="checkbox" />
             Item 2
           </label>
           <button className="btn btn-danger">Delete</button>
         </li>
       </ul>
     </>
-  );
+  )
 }
