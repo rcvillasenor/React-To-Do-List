92307833ce4b8f63ad90846fd606186f699d2c58
Richard Villasenor <richard.c.villasenor@gmail.com>
2023-04-29 10:30:14 -0700

    Splitting up components (newtodoform)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from "react";
 import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
+  function addTodo(title) {
     setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false },
-      ];
+      return [...currentTodos, { id: crypto.randomUUID(), completed: false }];
     });
-    setNewItem("");
   }
 
   function toggleTodo(id, completed) {
@@ -37,18 +31,7 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-            type="text"
-            id="item"
-          ></input>
-        </div>
-        <button className="btn">Add</button>
-      </form>
+      <NewTodoForm onSubmit={addTodo}/>
       <h1 className="header">To-do List</h1>
       <ul>
         {todos.length === 0 && "No Todos"}
